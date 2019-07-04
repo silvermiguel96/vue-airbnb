8ae2af585e94ff4d82d3b9660f313ba60657e21a
Miguelangel <silvermiguel96@gmail.com>
2019-07-04 10:25:41 -0500

    install and configure the database firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 import Vue from 'vue';
+import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+
 Vue.config.productionTip = false;
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBSKsVorv86I4h4B87JacgEpI1SmjMlCrA',
+  authDomain: 'platzi-rooms-363a6.firebaseapp.com',
+  databaseURL: 'https://platzi-rooms-363a6.firebaseio.com',
+  projectId: 'platzi-rooms-363a6',
+  storageBucket: '',
+  messagingSenderId: '260777805007',
+  appId: '1:260777805007:web:9007f8177483541d',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
