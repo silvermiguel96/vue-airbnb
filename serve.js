4e2e2bb197397997f5487d558a8db3c63d5bd141
Miguelangel <silvermiguel96@gmail.com>
2019-07-08 06:27:01 -0500

    Serve

--- serve.js
+++ serve.js
@@ -1,12 +1,13 @@
 const express = require('express');
+
 const port = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.static(__dirname + "/dist/"));
 
-app.get(/.*/, function (req, res) {
-  res.sendFile(__dirname, + "/dist/index.html")
+app.get(/.*/, function(req, res) {
+  res.sendFile(__dirname, + "/dist/index.html");
 });
 
 app.listen(port);
